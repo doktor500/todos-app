adefda55f3ccaf5021d16a1d1cdb698e37b4af71
David Molinero <doktor500@users.noreply.github.com>
2024-09-26 23:00:52 +0100

    Add new todo section

--- src/app/users/[id]/todos/page.tsx
+++ src/app/users/[id]/todos/page.tsx
@@ -1,4 +1,6 @@
-import { Checkbox } from "@/components/ui/checkbox";
+import { PlusIcon } from "lucide-react";
+
+import { Checkbox } from "@/ui/Checkbox";
 import { usersRepository } from "@/modules/infrastructure/repositories/usersDBRepository";
 
 type Params = { params: { id: number } };
@@ -10,11 +12,25 @@ const Page = async ({ params }: Params) => {
   return (
     <>
       <h1 className="pt-12 text-center text-2xl font-bold">Todo list</h1>
-      <div className="flex items-center justify-center pt-3">
+      <div className="flex cursor-pointer items-center justify-center pt-3">
         <ul>
+          <li>
+            <div className="h-11 w-96 rounded-sm bg-black/20 pl-4 pt-3 hover:bg-black/10 dark:bg-white/5 dark:hover:bg-white/30">
+              <div className="flex items-center text-gray-800 dark:text-white">
+                <PlusIcon size="20" />
+                <form>
+                  <input
+                    type="text"
+                    placeholder="Add a to-do..."
+                    className="w-80 border-none bg-transparent pl-1 text-sm outline-none"
+                  />
+                </form>
+              </div>
+            </div>
+          </li>
           {user.todos.map((todo) => (
             <li key={todo.id} className="pt-1">
-              <div className="h-11 w-96 rounded-sm bg-black/20 pl-4 pt-3 dark:bg-white/20">
+              <div className="h-11 w-96 rounded-sm bg-black/20 pl-4 pt-3 hover:bg-black/10 dark:bg-white/20 dark:hover:bg-white/40">
                 <div className="flex items-center text-gray-800 dark:text-white">
                   <Checkbox id={`todo-${todo.id}`} />
                   <label htmlFor={`todo-${todo.id}`} className="pl-2 text-sm">

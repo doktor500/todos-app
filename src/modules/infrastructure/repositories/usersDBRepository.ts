7722bffa96e90e64c724ec5474bf86bd15a4791c
David Molinero <doktor500@users.noreply.github.com>
2024-09-27 09:06:51 +0100

    Refactor query

--- src/modules/infrastructure/repositories/usersDBRepository.ts
+++ src/modules/infrastructure/repositories/usersDBRepository.ts
@@ -1,26 +1,16 @@
 import { eq } from "drizzle-orm";
 
 import UsersRepository from "@/modules/application/repositories/usersRepository";
-import { Todo } from "@/modules/domain/todo";
 import { User } from "@/modules/domain/user";
 import { Optional } from "@/modules/domain/utils/optional";
 import { db } from "@/modules/infrastructure/db";
 
 export const usersRepository: UsersRepository = {
   findById: async (id: number): Promise<Optional<User>> => {
-    const user = await db.query.UsersTable.findFirst({
+    return db.query.UsersTable.findFirst({
       columns: { id: true, name: true },
-      with: { todos: true },
+      with: { todos: { columns: { id: true, content: true } } },
       where: (user) => eq(user.id, id),
     });
-
-    return user && { ...user, todos: user.todos.map(toTodo) };
   },
-};
-
-const toTodo = <T extends Todo>(todo: T) => {
-  return {
-    id: todo.id,
-    content: todo.content,
-  };
 };

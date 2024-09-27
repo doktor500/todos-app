6c77fbd4ee900fae1881ab1a7043435f7fcfdd67
David Molinero <doktor500@users.noreply.github.com>
2024-09-27 15:33:10 +0100

    Make entire row clickable

--- src/components/app/TodoCheckbox.tsx
+++ src/components/app/TodoCheckbox.tsx
@@ -12,13 +12,12 @@ type Props = {
 
 export const TodoCheckbox = ({ userId, todoId, content, completed }: Props) => {
   return (
-    <div className="h-11 w-96 rounded-sm bg-black/20 pl-4 pt-3 hover:bg-black/10 dark:bg-white/20 dark:hover:bg-white/40">
+    <div
+      className="h-11 w-96 rounded-sm bg-black/20 pl-4 pt-3 hover:bg-black/10 dark:bg-white/20 dark:hover:bg-white/40"
+      onClick={() => toggleTodo({ userId, todoId, completed: !completed })}
+    >
       <div className="flex items-center text-gray-800 dark:text-white">
-        <Checkbox
-          id={`todo-${todoId}`}
-          checked={completed}
-          onClick={() => toggleTodo({ userId, todoId, completed: !completed })}
-        />
+        <Checkbox id={`todo-${todoId}`} checked={completed} />
         <label htmlFor={`todo-${todoId}`} className="pl-2 text-sm">
           {content}
         </label>

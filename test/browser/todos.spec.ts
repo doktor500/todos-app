6dbf0d0b50d87df483ce53d90b4ca317fbecdd31
David Molinero <doktor500@users.noreply.github.com>
2024-10-07 07:27:13 +0100

    Add browser test search filter + edit todo

--- test/browser/todos.spec.ts
+++ test/browser/todos.spec.ts
@@ -2,17 +2,38 @@ import { expect, test } from "@playwright/test";
 
 const userId = 1;
 
+test.describe.configure({ mode: "serial" });
+
 test("Can create and delete a todo", async ({ page, baseURL }) => {
   await page.goto(`${baseURL}/users/${userId}/todos`);
 
-  await page.getByLabel("New todo").fill("Todo 1");
+  await page.getByLabel("New todo").fill("Buy coffee");
   await page.getByLabel("New todo").press("Enter");
   await page.getByLabel("Loading spinner").waitFor({ state: "hidden" });
 
-  await expect(page.getByRole("textbox", { name: "Todo 1" })).toBeVisible();
+  await expect(page.getByRole("textbox", { name: "Buy coffee" })).toBeVisible();
 
   await page.getByLabel("Delete todo").click();
   await page.getByLabel("Loading spinner").waitFor({ state: "hidden" });
 
-  await expect(page.getByRole("textbox", { name: "Todo 1" })).not.toBeVisible();
+  await expect(page.getByRole("textbox", { name: "Buy coffee" })).not.toBeVisible();
+});
+
+test("Filter is taken into account when a todo is edit", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}/users/${userId}/todos`);
+
+  await page.getByLabel("New todo").fill("Buy pizza");
+  await page.getByLabel("New todo").press("Enter");
+  await page.getByLabel("Loading spinner").waitFor({ state: "hidden" });
+
+  await expect(page.getByRole("textbox", { name: "Buy pizza" })).toBeVisible();
+  await page.getByRole("searchbox").fill("pizza");
+
+  await page.getByRole("textbox", { name: "Buy pizza" }).fill("Buy wine");
+  await page.getByRole("searchbox").click();
+
+  await expect(page.getByRole("textbox", { name: "Buy wine" })).not.toBeVisible();
+
+  await page.getByRole("searchbox").fill("");
+  await expect(page.getByRole("textbox", { name: "Buy wine" })).toBeVisible();
 });

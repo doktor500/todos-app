e590b08310cf7be5b251720177a0b18d1a06e23a
David Molinero <doktor500@users.noreply.github.com>
2024-10-13 10:40:53 +0100

    Add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,37 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "X Todo",
   description: "X Todo application",
 };
 
+const sfProDisplay = localFont({
+  src: [
+    {
+      path: "../../public/fonts/SF-Pro-Display-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/SF-Pro-Display-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-sf-pro-display",
+});
+
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <body className="h-screen bg-gradient-to-b from-slate-900 to-slate-800 antialiased">
+      <body
+        className={cn("h-screen bg-gradient-to-b from-slate-900 to-slate-700/50 antialiased", sfProDisplay.variable)}
+      >
         {children}
         <Analytics />
         <SpeedInsights />

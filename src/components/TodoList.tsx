8bff176caa19aaa89f5aa74da1e59bb60665f289
David Molinero <doktor500@users.noreply.github.com>
2024-10-18 07:50:32 +0100

    Change to ordered list

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,12 +7,12 @@ export const TodoList = () => {
   const { getTodos } = useTodos();
 
   return (
-    <ul>
+    <ol>
       {getTodos().map(({ id, content, completed, stale }) => (
         <li key={id} className="pt-1">
           <TodoEntry todoId={id} content={content} completed={completed} stale={Boolean(stale)} />
         </li>
       ))}
-    </ul>
+    </ol>
   );
 };

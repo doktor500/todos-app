d6994b5bedf749d21b86ce5fd7aa4206056502a3
David Molinero <doktor500@users.noreply.github.com>
2024-10-26 20:03:10 +0100

    Add sign up browser test

---
+++ test/browser/signup.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "@playwright/test";
+
+test("a user can sign up successfully", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}/users/signup`);
+
+  await page.getByLabel("User name").fill("sarah");
+  await page.getByLabel("Email").fill("sarah@email.com");
+  await page.getByLabel("Password").fill("12345678");
+  await page.getByRole("button", { name: "Sign Up" }).click();
+
+  await page.waitForURL(/todos$/);
+  await expect(page.getByLabel("New todo")).toBeVisible();
+});

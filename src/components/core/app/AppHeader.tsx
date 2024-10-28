a6a578f5961612e9c5260dc46ac6864c2a15231d
David Molinero <doktor500@users.noreply.github.com>
2024-10-28 07:53:55 +0000

    Make app title dynamic

--- src/components/core/app/AppHeader.tsx
+++ src/components/core/app/AppHeader.tsx
@@ -2,14 +2,14 @@ import { AppLogo } from "@/components/core/app/appLogo";
 import { AppSpinner } from "@/components/core/app/appSpinner";
 import { AppTitle } from "@/components/core/app/appTitle";
 
-export const AppHeader = ({ isPending }: { isPending?: boolean }) => {
+export const AppHeader = ({ title, isPending }: { title?: string; isPending?: boolean }) => {
   return (
     <div className="flex items-center justify-center pb-6 pt-4">
       <div className="h-8">
         <AppSpinner isPending={isPending ?? false} />
         <AppLogo />
       </div>
-      <AppTitle />
+      <AppTitle title={title} />
     </div>
   );
 };
--- src/components/core/app/AppTitle.tsx
+++ src/components/core/app/AppTitle.tsx
@@ -1,7 +1,9 @@
-export const AppTitle = () => {
+export const AppTitle = ({ title }: { title?: string }) => {
   return (
-    <h1 className="bg-gradient-to-r from-custom-primary to-custom-secondary bg-clip-text pl-4 pt-2 text-center font-sans text-2xl font-bold text-transparent">
-      Inbox
-    </h1>
+    title && (
+      <h1 className="bg-gradient-to-r from-custom-primary to-custom-secondary bg-clip-text pl-4 pt-2 text-center font-sans text-2xl font-bold text-transparent">
+        {title}
+      </h1>
+    )
   );
 };

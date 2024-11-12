dd6bede9760cdf62646caf5b232c2856894a6a7e
David Molinero <doktor500@users.noreply.github.com>
2024-11-12 14:47:07 +0000

    Improve scroll area

--- src/components/core/todos/todoList.tsx
+++ src/components/core/todos/todoList.tsx
@@ -8,8 +8,8 @@ export const TodoList = () => {
   const { getTodos } = useTodos();
 
   return (
-    <ScrollArea className="mt-6 h-[calc(100vh-292px)] rounded-md">
-      <ol>
+    <ScrollArea className="mt-6 h-[calc(100vh-292px)] w-[calc(100%+1rem)] rounded-md">
+      <ol className="pr-4">
         {getTodos().map(({ id, content, completed, stale }) => (
           <li key={id} className="[&:not(:first-child)]:pt-1">
             <TodoEntry todoId={id} content={content} completed={completed} stale={Boolean(stale)} />

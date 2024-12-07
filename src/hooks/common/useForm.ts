546589514852672eb554fdc588c988058782f1f8
David Molinero <doktor500@users.noreply.github.com>
2024-12-07 18:47:52 +0000

    Avoid autofocus on mobile devices

--- src/hooks/common/useForm.ts
+++ src/hooks/common/useForm.ts
@@ -1,10 +1,13 @@
 import { useRef, useState } from "react";
+import { isMobile } from "react-device-detect";
 
 export const useForm = () => {
   const formRef = useRef<HTMLFormElement>(null);
   const [pending, setPending] = useState(false);
 
-  const resetForm = (): void => formRef.current?.reset();
+  const resetForm = (): void => {
+    if (!isMobile) formRef.current?.reset();
+  };
 
   return { formRef, pending, setPending, resetForm };
 };

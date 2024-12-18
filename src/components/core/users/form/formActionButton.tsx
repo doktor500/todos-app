f1fccebd23ed80d0e6b73429380082469bb84ece
David Molinero <doktor500@users.noreply.github.com>
2024-12-18 18:55:58 +0000

    Use primary and secondary colors

--- src/components/core/users/form/formActionButton.tsx
+++ src/components/core/users/form/formActionButton.tsx
@@ -4,7 +4,7 @@ export const FormActionButton = ({ isLoading, value }: { isLoading: boolean; val
   return (
     <Button
       type="submit"
-      className="group relative mb-2 me-2 inline-flex w-full items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-blue-500 to-purple-600 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
+      className="group relative mb-2 me-2 inline-flex w-full items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-custom-primary to-custom-secondary p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 group-hover:from-purple-600 group-hover:to-blue-500 dark:text-white dark:focus:ring-blue-800"
       disabled={isLoading}
     >
       <span className="relative w-full rounded-md bg-white px-5 py-1.5 transition-all duration-75 ease-in group-hover:opacity-90 dark:bg-gray-900">
